92e7c2baa0b98620ba7d589809614718944c8bfe
David Spörri <davepermen@hotmail.com>
2025-01-23 21:16:49 +0100

    ok

--- csft-lib.cpp
+++ csft-lib.cpp
@@ -13,6 +13,7 @@ void csft_setup(String name, void (*configure_wifisettings_parameters)())
     Serial.println(name);
     pinMode(LED_BUILTIN, OUTPUT);
     LittleFS.begin();
+    WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE);
     WiFi.hostname(name.substring(0, 32).c_str());
     WiFi.setPhyMode(WIFI_PHY_MODE_11G);
     WiFi.forceSleepBegin();
