2c169b7901a464a67a5c90dfcc7cfdb5c33997a0
David Spörri <davepermen@hotmail.com>
2025-01-23 15:33:29 +0100

    hope this works :)

--- csft-lib.cpp
+++ csft-lib.cpp
@@ -4,10 +4,13 @@
 #include <ESP8266WiFi.h>
 #include <WiFiSettings.h>
 #include <LittleFS.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
 DoubleResetDetector drd(10, 0);
 
-void csft_setup(char *name, void (*configure_wifisettings_parameters)()) {
+void csft_setup(char *name, void (*configure_wifisettings_parameters)())
+{
     Serial.begin(115200);
     Serial.print("booting up ");
     Serial.println(name);
@@ -20,13 +23,33 @@ void csft_setup(char *name, void (*configure_wifisettings_parameters)()) {
     WiFiSettings.hostname = "csft-device-";
     configure_wifisettings_parameters();
 
-    if (drd.detectDoubleReset()) {
-    WiFiSettings.portal();
+    if (drd.detectDoubleReset())
+    {
+        WiFiSettings.portal();
     }
 
     WiFiSettings.connect();
 }
 
-void csft_loop() {
+void csft_loop()
+{
     drd.loop();
+}
+
+void csft_web_request(String url, String name, String id, void (*process_response)(HTTPClient &http) = 0)
+{
+    WiFiClient wificlient;
+    HTTPClient http;
+
+    if (http.begin(wificlient, webRequest))
+    {
+        http.setUserAgent(name);
+        http.addHeader(name + "-Id", id);
+        http.GET();
+        if (process_response != 0)
+        {
+            process_response(http);
+        }
+        http.end();
+    }
 }
--- csft-lib.h
+++ csft-lib.h
@@ -5,4 +5,6 @@ void csft_setup(char *name, void (*configure_wifisettings_parameters)());
 
 void csft_loop();
 
+void csft_web_request(String url, String name, String id, void (*process_response)(HTTPClient &http) = 0);
+
 #endif
